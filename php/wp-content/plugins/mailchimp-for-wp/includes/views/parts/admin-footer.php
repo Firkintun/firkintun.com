f554ad2e0ff95884b094c91f035cdb743bbb0cb2
Thomas Beirne <thomas@blossor.com>
2015-01-10 11:46:00 -0800

    Replace WP page with simple HTML file.

--- php/wp-content/plugins/mailchimp-for-wp/includes/views/parts/admin-footer.php
+++
@@ -1,3 +0,0 @@
-<br style="clear:both;" />
-<p class="copyright-notice">This plugin is not developed by or affiliated with MailChimp in any way.</p>
-<p class="copyright-notice">Enjoying MailChimp for WP Lite? <a href="http://dannyvankooten.com/wordpress-plugins/mailchimp-for-wordpress/">Upgrade to MailChimp for WordPress Pro now</a>. It's awesome, you'll love it!</p>
--- update.sh
+++
@@ -1,7 +0,0 @@
-pushd php
-git pull
-popd
-git add .
-git commit -m "Update Wordpress PHP from upstream."
-git push origin HEAD
-
